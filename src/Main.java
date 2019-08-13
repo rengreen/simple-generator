88a34e95faa14886e6d897d1ecc0951fc9c68bde
rengreen <r.m.gardzinska@gmail.com>
2019-08-13 14:03:03 +0200

    Add deleting of person from initial list

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     private static final int STARTERKA_POINTS = 5;
@@ -15,6 +16,81 @@ public class Main {
         String[] juniorki = new String[]{"juniorka1", "juniorka2", "juniorka3", "juniorka4", "juniorka5"};
         String[] szeregowe = new String[]{"szeregowa1", "szeregowa2", "szeregowa3", "szeregowa4", "szeregowa5", "szeregowa6", "szeregowa7"};
         String[] komandoski = new String[]{"komandoska1", "komandoska2", "komandoska3"};
+
+        System.out.println("starterki " + Arrays.toString(starterki));
+        System.out.println("juniorki " + Arrays.toString(juniorki));
+        System.out.println("szeregowe " + Arrays.toString(szeregowe));
+        System.out.println("komandoski " + Arrays.toString(komandoski));
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Czy chcesz usunąć(u) lub dodać(d) osobę?");
+        System.out.println("lub wciśnij enter, aby przejść dalej.");
+
+        String answer = scanner.nextLine();
+        if (answer.equals("u")) {
+            System.out.println("Wybierz kategorię: komandoska(k), szeregowa(z), juniorka(j), starterka(s)");
+            System.out.println("lub wciśnij enter, aby zrezygnować.");
+            String answerDelete = scanner.nextLine();
+
+            switch (answerDelete) {
+                case "k": //komandoski
+                    System.out.println("Wybierz osobę: ");
+                    for (int i = 0; i < komandoski.length; i++) {
+                        System.out.println(komandoski[i] + " (" + (i + 1) + ")");
+                    }
+                    String answerPerson = scanner.nextLine();
+                    int personNumber = Integer.parseInt(answerPerson) - 1;
+                    if (personNumber >= 0 && personNumber < komandoski.length) {
+                        komandoski = deleteOneGirl(komandoski, personNumber);
+                        System.out.println("po usunięciu: komandoski " + Arrays.toString(komandoski));
+                    }
+                    break;
+                case "z": //szeregowe
+                    System.out.println("Wybierz osobę: ");
+                    for (int i = 0; i < szeregowe.length; i++) {
+                        System.out.println(szeregowe[i] + "(" + (i + 1) + ")");
+                    }
+                    answerPerson = scanner.nextLine();
+                    personNumber = Integer.parseInt(answerPerson) - 1;
+                    if (personNumber >= 0 && personNumber < szeregowe.length) {
+                        szeregowe=deleteOneGirl(szeregowe, personNumber);
+                        System.out.println("po usunięciu: szeregowe " + Arrays.toString(szeregowe));
+                    }
+                    break;
+                case "j": //juniorki
+                    System.out.println("Wybierz osobę: ");
+                    for (int i = 0; i < juniorki.length; i++) {
+                        System.out.println(juniorki[i] + "(" + (i + 1) + ")");
+                    }
+                    answerPerson = scanner.nextLine();
+                    personNumber = Integer.parseInt(answerPerson) - 1;
+                    if (personNumber >= 0 && personNumber < juniorki.length) {
+                        juniorki=deleteOneGirl(juniorki, personNumber);
+                        System.out.println("po usunięciu: juniorki " + Arrays.toString(juniorki));
+                    }
+                    break;
+                case "s": //starterki
+                    System.out.println("Wybierz osobę: ");
+                    for (int i = 0; i < starterki.length; i++) {
+                        System.out.println(starterki[i] + "(" + (i + 1) + ")");
+                    }
+                    answerPerson = scanner.nextLine();
+                    personNumber = Integer.parseInt(answerPerson) - 1;
+                    if (personNumber >= 0 && personNumber < starterki.length) {
+                        starterki=deleteOneGirl(starterki, personNumber);
+                        System.out.println("po usunięciu: starterki " + Arrays.toString(starterki));
+                    }
+                    break;
+
+                default:
+                    throw new IllegalStateException("Nieprawidłowa odpowiedź: " + answerDelete);
+            }
+        } else if (answer.equals("d")) {
+            System.out.println("Wybierz kategorię: komandoska(k), szeregowa(z), juniorka(j), starterka(s)");
+            System.out.println("Wciśnij enter, aby zrezygnować.");
+            String answerAdd = scanner.nextLine();
+        }
+
 
         //pomieszanie osób
         String[] starterkiMix = shuffleArray(starterki);
@@ -81,10 +157,10 @@ public class Main {
         String[] zonyHollywood = collectGroup("zonyHollywood", allGroups, allGirls);
         String[] kosmonautki = collectGroup("kosmonautki", allGroups, allGirls);
 
-        System.out.println("Wylosowano gamerki: " + Arrays.toString(gamerki)+" pkt: "+countPoints("gamerki", allGroups, allPoints));
-        System.out.println("Wylosowano heroski: " + Arrays.toString(heroski)+" pkt: "+countPoints("heroski", allGroups, allPoints));
-        System.out.println("Wylosowano żony Hollywood: " + Arrays.toString(zonyHollywood)+" pkt: "+countPoints("zonyHollywood", allGroups, allPoints));
-        System.out.println("Wylosowano kosmonautki: " + Arrays.toString(kosmonautki)+" pkt: "+countPoints("kosmonautki", allGroups, allPoints));
+        System.out.println("Wylosowano gamerki: " + Arrays.toString(gamerki) + " pkt: " + countPoints("gamerki", allGroups, allPoints));
+        System.out.println("Wylosowano heroski: " + Arrays.toString(heroski) + " pkt: " + countPoints("heroski", allGroups, allPoints));
+        System.out.println("Wylosowano żony Hollywood: " + Arrays.toString(zonyHollywood) + " pkt: " + countPoints("zonyHollywood", allGroups, allPoints));
+        System.out.println("Wylosowano kosmonautki: " + Arrays.toString(kosmonautki) + " pkt: " + countPoints("kosmonautki", allGroups, allPoints));
     }
 
     //losowe pomieszanie tablicy
@@ -121,7 +197,7 @@ public class Main {
     public static int countPoints(String groupName, String[] allGroups, int[] allPoints) {
         int points = 0;
         for (int i = 0; i < NUMBER_OF_GIRLS; i++) {
-            if (allGroups[i]!=null && allGroups[i].equals(groupName)) {
+            if (allGroups[i] != null && allGroups[i].equals(groupName)) {
                 points += allPoints[i];
             }
         }
@@ -131,13 +207,28 @@ public class Main {
     //suma puntów za zapisane dotychczas uczestniczki
     public static String[] collectGroup(String groupName, String[] allGroups, String[] allGirls) {
         String[] group = new String[5];
-        int position=0;
+        int position = 0;
         for (int i = 0; i < NUMBER_OF_GIRLS; i++) {
-            if (allGroups[i]!=null && allGroups[i].equals(groupName)) {
-                group[position]= allGirls[i];
+            if (allGroups[i] != null && allGroups[i].equals(groupName)) {
+                group[position] = allGirls[i];
                 position++;
             }
         }
         return group;
+    }
+
+    public static String[] deleteOneGirl(String[] categoryList, int position) {
+        int arrayLength = categoryList.length;
+        String[] newCategoryList = new String[arrayLength - 1];
+
+        for (int i = 0; i < arrayLength; i++) {
+            if (i < position) {
+                newCategoryList[i] = categoryList[i];
+            } else if (i > position) {
+                newCategoryList[i - 1] = categoryList[i];
+            }
+        }
+
+        return newCategoryList;
     }
 }
